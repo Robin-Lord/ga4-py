9ce3240e4e7cfd592ace0bf2ee50433e4ce94bbb
robin.lord <robin.lord@aira.net>
2024-01-10 11:05:39 +0000

    refactor(dependencies): :heavy_minus_sign: Remove unneeded dependencies

--- setup.py
+++ setup.py
@@ -9,9 +9,5 @@ setup(
     install_requires=[
         "ga4mp==2.0.4",
         "requests==2.25.1",
-        "google-api-python-client==2.5.0",
-        "google-auth==1.30.0",
-        "google-auth-httplib2==0.1.0",
-        "google-auth-oauthlib==0.4.4"
     ],
 )
