4b2a42591fd57ae535cc86b468d89d306ef35276
robin.lord <robin.lord@aira.net>
2024-08-27 18:32:16 +0800

    build(dependencies): :arrow_up: Ensure requests dependency is upgraded in setup.py and update version name

--- setup.py
+++ setup.py
@@ -1,14 +1,14 @@
-from setuptools import setup #type: ignore
+from setuptools import setup  # type: ignore
 
 setup(
     name="ga4py",
-    version="1.0.0",
+    version="1.1.0",
     description="Python package to simplify adding GA4 tracking to Python functions",
     author="Aira Innovations",
     packages=["ga4py"],
     install_requires=[
         "ga4mp==2.0.4",
-        "requests==2.25.1",
+        "requests==2.32.3",
     ],
-    package_data={'ga4py': ['py.typed']},
+    package_data={"ga4py": ["py.typed"]},
 )
